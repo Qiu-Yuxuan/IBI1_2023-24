a596194013d9a0540d230fc1156ec66716049a61
YuxuanQiu <860594573@qq.com>
2024-03-08 12:28:26 +0800

    renamefolder

---
+++ Practical4/comparaingtraining.py
@@ -0,0 +1,25 @@
+#training by both running and strength exercises is better
+#a = 40
+#b = 36
+#c = 30
+# caculate the difference
+#d = a - b
+#e = b - c
+# compare difference value
+#if d > e：
+ # output "Training only by running is better."
+#if d < e：
+  #output"Training by both running and strength exercises is better."
+#else：
+  #output "Both training are equally good."
+a=40
+b=36
+c=30
+d=a-b
+e=b-c
+if d>e:
+  print("Training only by running is better.")
+elif d<e:
+  print("Training by both running and strength exercises is better.") 
+else:
+  print("Both training are equally good.") 
