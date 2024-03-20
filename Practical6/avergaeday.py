adcc574618501839db10089618aa102543f718c8
YuxuanQiu <860594573@qq.com>
2024-03-20 14:11:56 +0800

    practical6 modified

--- Practical6/avergaeday.py
+++ Practical6/avergaeday.py
@@ -1,9 +1,10 @@
-average_day={'sleeping':8, 'classes':6, 'studying':3.5, 'TV':2, 'music':1,'other':3.5}
+average_day={'sleeping':8, 'classes':6, 'studying':3.5, 'TV':2, 'music':1,'other':3.5}#it is the dictionart that record the data
 print(average_day)
 import matplotlib.pyplot as plt
-activity_lablels = ["sleeping", "classes", "studying", "TV", "Music", "other"]
-time_day =[8, 6, 3.5, 2, 1, 3.5]
+activity_lablels = ["sleeping", "classes", "studying", "TV", "Music", "other"]#set the abscissa 
+time_day =[8, 6, 3.5, 2, 1, 3.5] #set the ordinate
 plt.figure()
-plt.pie(time_day, labels=activity_lablels, startangle= 90)
-plt.show()
-plt.clf
+plt.pie(time_day, labels=activity_lablels, startangle= 90)#set the data and tyle of the figure
+plt.title("the average day of a university students")#set the title of the figure
+plt.show()#show the figure
+plt.clf#close the figure
--- Practical6/city_size.py
+++ Practical6/city_size.py
@@ -1,22 +1,22 @@
-uk_cities=[0.56,0.62,0.04,9.7]
-china_cities=[0.58,8.4,29.9,22.2]
+uk_cities=[0.56,0.62,0.04,9.7]#store the population sizes
+china_cities=[0.58,8.4,29.9,22.2]#store the population sizes
 print(uk_cities)
 print(china_cities)
 import numpy as np
 import matplotlib.pyplot as plt
-uk_cityname=["Edinburgh","Glasgow","Stirling","London"]
-uk_populations=uk_cities
-width=0.5
+uk_cityname=["Edinburgh","Glasgow","Stirling","London"]#set the abscissa
+uk_populations=uk_cities#set the ordinate
+width=0.5#set the width
 plt.figure()
 plt.bar(uk_cityname,uk_populations,width)
-plt.ylabel("populations")
-plt.title("populations of uk cities")
-plt.show()
-china_cityname=["Haining","Hangzhou","Shanghai","Beijing"]
-china_populations=china_cities
+plt.ylabel("populations")#set the y axis label
+plt.title("populations of uk cities")#set the title
+plt.show()#show the figure
+china_cityname=["Haining","Hangzhou","Shanghai","Beijing"]#set the abscissa
+china_populations=china_cities#set the ordinate
 plt.figure()
-plt.bar(china_cityname,china_populations,width)
-plt.ylabel("populations")
-plt.title("populations of china cities")
-plt.show()
-plt.clf
+plt.bar(china_cityname,china_populations,width)#set the style and data
+plt.ylabel("populations")#set the y axis label
+plt.title("populations of china cities") #set the title
+plt.show()#show the figure
+plt.clf#close the figure
